a89a8408abb43b2be3a3a4dc402b9f44617bf459
Stanley F <stanley.foerster@gmail.com>
2025-06-02 07:12:47 +0200

    [WIP] rework plugin architecture

--- plugins/rlgym/rlgym.py
+++ plugins/rlgym/rlgym.py
@@ -1,9 +1,11 @@
 import logging
-from typing import Any
+from typing import Any, override
 
 import gymnasium as gym
-from bluesky import core, stack
-from gymnasium import Env
+import numpy as np
+from bluesky import core, sim, stack, traf
+from bluesky_gym.envs import BlueSkyEnv
+from gymnasium import registry
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -22,30 +24,93 @@ class BlueSkyGym(core.Entity):
     def __init__(self) -> None:
         super().__init__()
         logger.info("Welcome to the Gym!")
-        self._env: Env | None = None
+        self._env: BlueSkyEnv | None = None
+        self._observation: dict | None = None
+        self._last_radar_update = -np.inf
+        self._action_duration = None
 
-    @core.timed_function(name="rlloop", dt=5)
-    def update(self) -> None:
-        if not self._env:
+    @core.timed_function(name="rl_action", hook="preupdate")
+    def perform_action(self) -> None:
+        if self._env is None or self._observation is None:
             return
 
-        # TODO move this into control of the gym lib.
-        # There, provide the possibility to attach RL algorithms.
+        if sim.simt - self._last_radar_update < 5:  # TODO - move this to the environment to allow it to decide for the time step to use
+            self._action_duration = None
+            return
+
+        logger.info(f"Radar updated at {sim.simt}. Performing Action!")
+        self._last_radar_update = sim.simt
+
+        # TODO - call the library (aka the "agent") to decide which algorithm to use for
+        # selecting an action
         action = self._env.action_space.sample()
-        observation, reward, terminated, truncated, info = self._env.step(action)
+        self._action_duration = self._env.perform_action(action)
+
+    # in between preupdate and update hook, the simulation kicks in and updates traffic.
+
+    @core.timed_function(name="rl_reward", hook="update")
+    def collect_reward_and_observation(self) -> None:
+        if self._env is None:
+            return
+
+        if self._action_duration is None:
+            return
+
+        logger.info(f"Collecting rewards and obtain new observation at {sim.simt}.")
+
+        self._observation = self._env.get_observation()
+        terminated, final_reward = self._env._terminated()
+        if terminated:
+            logger.info("Environment terminated.")
+        # reward = final_reward - action_duration  # penalty for each action is equal to
+        # its duration
+        truncated = self._env._truncated()
+        if truncated:
+            logger.info("Environment truncated.")
+        # info = {}
         if terminated or truncated:
-            self._env.reset()
+            logger.info("Resetting the traffic object and environment")
+            # official bs gym environment does not reset the full simulation, but instead does:
+            # this call could be part of env.reset, since it is the same for all render_modes.
+            traf.reset()  # this calls reset of all the TrafficArray classes (also this one), so no need to self.reset
+            # self._observation, _ = self._env.reset()
+            # self._last_radar_update = -np.inf
+            # # self.reset()
 
     @stack.command
-    def train(self, environment=None):
+    def train(self, environment: str | None = None, algorithm: str | None = None):
         if environment is None:
             if self._env:
                 self._env.close()
                 self._env = None
+            else:
+                return (
+                    True,
+                    f"Environments available: {
+                        [
+                            env
+                            for env in registry
+                            if env.startswith('bluesky_gym/')
+                        ]
+                    }",
+                )
         else:
             self._env = gym.make(
-                f"bluesky_gym/{gym}",
+                f"bluesky_gym/{environment}",
+                render_mode="plugin",
                 time_step=5,
-            )
-            self._env.reset()
-        return True, f"Training in the gym is {'dis' if self._env is None else 'en'}abled."
+            )  # type: ignore
+            self._observation, _ = self._env.reset()
+            self._last_radar_update = -np.inf
+        return (
+            True,
+            f"Training in the gym is {'dis' if self._env is None else 'en'}abled.",
+        )
+
+    @override
+    def reset(self) -> None:
+        logger.info("Resetting the Gym.")
+        if self._env is not None:
+            self._last_radar_update = -np.inf
+            self._action_duration = None
+            self._observation, _ = self._env.reset()
